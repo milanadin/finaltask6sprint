ad96762dfdcff411044c956f7808555477965653
Milana Din <milanadin@gmail.com>
2025-11-12 18:03:04 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "INFO: ", log.LstdFlags)
+
+	srv := server.NewServer(logger)
+	logger.Println("Сервер запущен на порту 8080...")
+	if err := srv.Server.ListenAndServe(); err != nil {
+		logger.Fatal(err)
+	}
 }
